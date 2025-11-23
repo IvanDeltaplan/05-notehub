7f1144e49199acc3a5da0e78dfa3b8f6aca79554
ivan nick <ivannikolaev777@gmail.com>
2025-11-22 22:34:48 -0500

    commit message update Add React Query Devtools and fix NoteForm fields and validation

---
+++ src/components/NoteList/NoteList.tsx
@@ -0,0 +1,21 @@
+// src/components/NoteList/NoteList.tsx
+import NoteCard from "../NoteCard/NoteCard";
+import css from "./NoteList.module.css";
+import type { Note } from "../../types/Note";
+
+interface NoteListProps {
+  notes: Note[];
+  onDelete: (id: string) => void;
+}
+
+export default function NoteList({ notes, onDelete }: NoteListProps) {
+  if (!notes.length) return <p>No notes yet</p>;
+
+  return (
+    <ul className={css.list}>
+      {notes.map((note) => (
+        <NoteCard key={note.id} note={note} onDelete={onDelete} />
+      ))}
+    </ul>
+  );
+}
---
+++ src/services/noteService.ts
@@ -0,0 +1,89 @@
+import axios, { type AxiosResponse } from "axios";
+import type { Note } from "./../types/Note";
+
+const BASE_URL = "https://notehub-public.goit.study/api";
+const token = import.meta.env.VITE_NOTEHUB_TOKEN as string;
+
+if (!token) {
+  // на випадок, якщо забудеш покласти токен у .env
+  console.warn("VITE_NOTEHUB_TOKEN is not set");
+}
+
+function getAuthHeaders() {
+  return {
+    Accept: "application/json",
+    Authorization: `Bearer ${token}`,
+  };
+}
+
+export interface FetchNotesParams {
+  search?: string;
+  tag?: string;
+  page?: number;
+  perPage?: number;
+  sortBy?: "created" | "updated";
+}
+
+export interface NotesResponse {
+  notes: Note[];
+  totalPages: number;
+  page?: number;
+  perPage?: number;
+  total?: number;
+}
+
+/** Отримати список нотаток */
+export async function fetchNotes(
+  params: FetchNotesParams
+): Promise<NotesResponse> {
+  const res: AxiosResponse<NotesResponse> = await axios.get(
+    `${BASE_URL}/notes`,
+    {
+      headers: getAuthHeaders(),
+      params,
+    }
+  );
+
+  return res.data;
+}
+
+/** Пейлоад для створення нотатки */
+export interface CreateNotePayload {
+  title: string;
+  content: string;
+  tag: string;
+}
+
+/** Створити нотатку */
+export async function createNote(
+  
+  payload: CreateNotePayload
+): Promise<Note> {
+
+   console.log("🔑 Token:", token ? "Present" : "Missing");
+  console.log("📦 Payload:", payload);
+  const res: AxiosResponse<Note> = await axios.post(
+    `${BASE_URL}/notes`,
+    payload,
+    {
+      headers: {
+        ...getAuthHeaders(),
+        "Content-Type": "application/json",
+      },
+    }
+  );
+
+  return res.data;
+}
+
+/** Видалити нотатку за id */
+export async function deleteNote(id: string): Promise<Note> {
+  const res: AxiosResponse<Note> = await axios.delete(
+    `${BASE_URL}/notes/${id}`,
+    {
+      headers: getAuthHeaders(),
+    }
+  );
+
+  return res.data;
+}
